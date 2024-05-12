be1c1954ed6e9dccf3dccb7fbb120d9441009f06
carsonkrueger <crkruege@gmail.com>
2024-05-12 14:15:41 -0600

    working on hmac

--- src/libs/lib-hash/error.rs
+++ src/libs/lib-hash/error.rs
@@ -3,10 +3,17 @@ pub type HashResult<T> = Result<T, HashError>;
 #[derive(Debug)]
 pub enum HashError {
     Argon2Error(argon2::password_hash::Error),
+    InvalidLength,
 }
 
 impl From<argon2::password_hash::Error> for HashError {
     fn from(value: argon2::password_hash::Error) -> Self {
         HashError::Argon2Error(value)
+    }
+}
+
+impl From<sha2::digest::InvalidLength> for HashError {
+    fn from(_value: sha2::digest::InvalidLength) -> Self {
+        Self::InvalidLength
     }
 }
--- src/libs/lib-hash/schemes/mod.rs
+++ src/libs/lib-hash/schemes/mod.rs
@@ -1,1 +1,2 @@
 pub mod argon2_v01;
+pub mod hmac_v01;
